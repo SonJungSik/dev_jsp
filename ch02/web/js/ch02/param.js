1879270dda9de649700d556ae2d6068579465f0b
dev_jsp <sonjs2001@naver.com>
2016-07-27 19:50:31 +0900

    ch02-07-28일

---
+++ ch02/web/js/ch02/param.js
@@ -0,0 +1,23 @@
+/**
+ * param.js
+ */
+function check(){
+	if (document.frm.id.value == "") {
+		alert("아이디를 입력해주세요.");
+		document.frm.id.focus();
+		return false;
+	} 
+	if (document.frm.age.value== "") {
+		alert("나이를 입력해주세요.");
+		document.frm.age.focus();
+		return false;
+	}
+	if (isNaN(document.frm.age.value)) {
+		alert("나이는 숫자로 입력해주세요.");
+		document.frm.age.value="";
+		document.frm.focus();
+		return false;
+	} 
+	return true;
+	
+}
