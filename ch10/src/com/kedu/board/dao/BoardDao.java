2f279a7297de684e7b762a9ce3a8421137fca0bd
dev_jsp <sonjs2001@naver.com>
2016-08-17 17:42:19 +0900

    8월 17일

--- ch10/src/com/kedu/board/dao/BoardDao.java
+++ ch10/src/com/kedu/board/dao/BoardDao.java
@@ -21,6 +21,9 @@ public class BoardDao {
 	private static BoardDao instance = new BoardDao();
 	
 	public static BoardDao getInstance() {
+		if(instance == null){
+			instance = new BoardDao();
+		}
 		return instance;
 	}
 	
